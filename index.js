cc0d82b832d52ab851d32612f5c31ae50daa225c
yash-23092001 <yash.chhelaiya@gmail.com>
2024-06-13 17:07:32 -0400

    added sound on click event and animation on buttons

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-var nextSequence = () => {
-    return Math.floor(Math.random() * 4);
-}
-var randomNumber = nextSequence();
-var buttonColors = ["red", "blue", "green", "yellow"];
-var randomChosenColor = buttonColors[randomNumber];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
-gamePattern.push(randomChosenColor);
-$("#"+randomChosenColor).fadeOut(100).fadeIn(100);
+var userClickedPattern = [];
+
+var playSound = (name) => {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+function nextSequence() {
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  //1. Use jQuery to select the button with the same id as the randomChosenColour
+  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  //3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+  playSound(randomChosenColour);
+
+}
+nextSequence();
+
+$(".btn").click((event) => {
+    var userChosenColor = event.target.id;
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+});
+
+function animatePress(color){
+    $(".btn").click((event) => {
+        event.target.classList.add("pressed");
+        setTimeout(()=>{
+            event.target.classList.remove("pressed");
+        }, 100);
+    });
+}
